7429688247bac5cf1d960168a0d9bc605be9d714
yesirsenic <yesirsenic@naver.com>
2026-01-12 17:57:41 +0900

    commit
    time_slider 제작

---
+++ TimeToMatch/Assets/Scripts/TimeSlider.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeSlider : MonoBehaviour
+{
+    
+    [SerializeField] private Slider slider;
+    [SerializeField] private float maxTime = 5f;
+    [SerializeField] private float smoothSpeed = 8f;
+
+    private float currentTime;
+
+    void Start()
+    {
+        currentTime = maxTime;
+        slider.value = 1f;
+    }
+
+    void Update()
+    {
+        // 실제 시간 감소
+        currentTime -= Time.deltaTime;
+        currentTime = Mathf.Max(currentTime, 0f);
+
+        float targetValue = currentTime / maxTime;
+
+        // UI는 부드럽게 따라감
+        slider.value = Mathf.Lerp(
+            slider.value,
+            targetValue,
+            Time.deltaTime * smoothSpeed
+        );
+    }
+}
